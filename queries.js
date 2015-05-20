b732b5041e53cc7da5e5ff3be26b9e778cc393f3
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-05-20 23:05:10 +1000

    Fixed and restructured broken queries

--- queries.js
+++ queries.js
@@ -1,5 +1,5 @@
 // Command to insert CSV with headers to Mongo:
-//mongoimport -d railway -c train --type csv --file ~/Downloads/test.csv --headerline
+//mongoimport -d railway -c train --type csv --file ~/Downloads/test.csv --headerline --ignoreBlanks
 
 /*
 Runs a basic map reduce to get the total distance travelled on each track
@@ -25,23 +25,72 @@ function totalDistanceMR() {
     reduceFunc,
     {
       out: 'total_distance',
-      query: { km:
-                { $gt: 0 }
-             },
+      query: { km: { $gt: 0 }},
       finalize: finalizeFunc
     }
   );
 }
 
+function fixTypes() {
+  db.train.find({Track: {$exists: true}}).forEach(function(doc) {
+    // Cast Track to be a Int32 type
+    doc.Track = new NumberInt(doc.Track);
+
+    // Write changes to DB
+    db.train.save(doc);
+  });
+
+  // Change LoadEmpty = 1 to 'loaded'
+  db.train.update(
+    // query
+    { "LoadEmpty" : 1 },
+
+    // update
+    { $set: {LoadEmpty: 'loaded'} },
+
+    // options
+    {
+      "multi" : true,  // update only one document
+      "upsert" : false  // insert a new document, if no existing document match the query
+    }
+  );
+
+  // Change LoadEmpty = 0 to 'empty'
+  db.train.update(
+    // query
+    { "LoadEmpty" : 0 },
+
+    // update
+    { $set: {LoadEmpty: 'empty'} },
+
+    // options
+    {
+      "multi" : true,  // update only one document
+      "upsert" : false  // insert a new document, if no existing document match the query
+    }
+  );
+
+  // Change LoadEmpty = -1 to 'unknown'
+  db.train.update(
+    // query
+    { "LoadEmpty" : -1 },
+
+    // update
+    { $set: {LoadEmpty: 'unknown'} },
+
+    // options
+    {
+      "multi" : true,  // update only one document
+      "upsert" : false  // insert a new document, if no existing document match the query
+    }
+  );
+}
 
 /*
 Iteratively restructure default documents into related fields
  */
-function preProcessing() {
+function changeStructure() {
   var updateDocsFunc = function(doc) {
-    // Change LoadEmpty attribute to boolean type
-    doc.LoadEmpty = doc.LoadEmpty === -1 ? false : true;
-
     // Add location nested doc
     doc.Loc = {
       Lat: doc.Lat,
@@ -95,43 +144,12 @@ function preProcessing() {
   db.train.find().snapshot().forEach(updateDocsFunc);
 
   // Create a geospatial index on Loc nested document
-  db.train.createIndex({'Loc': '2d'});
+  db.train.createIndex({Loc: '2d'});
 
   // Create an index on km field
-  db.train.createIndex({'km': 1});
+  db.train.createIndex({km: 1});
 
   // Find all documents stating a value less than 0 for KM travelled (these are obviously wrong)
-  db.train.find({'km': {'$lt': 0}});
+  db.train.find({km: {$lt: 0}});
 }
 
-function fixBools() {
-  // Change LoadEmpty = -1 to true
-  db.train.update(
-    // query
-    { "LoadEmpty" : 0 },
-
-    // update
-    { $set: {LoadEmpty: true} },
-
-    // options
-    {
-      "multi" : true,  // update only one document
-      "upsert" : false  // insert a new document, if no existing document match the query
-    }
-  );
-
-  // Change LoadEmpty = -1 to false
-  db.train.update(
-    // query
-    { "LoadEmpty" : -1 },
-
-    // update
-    { $set: {LoadEmpty: false} },
-
-    // options
-    {
-      "multi" : true,  // update only one document
-      "upsert" : false  // insert a new document, if no existing document match the query
-    }
-  );
-}
